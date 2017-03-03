a384df59bd7389c8d87f6bea2f69ea2ba6864fce
Robin Chavignat <r.chavignat@gmail.com>
2017-03-03 09:33:05 +0100

    fix(udev): add orbbec udev rules file

--- Packaging/Linux/install.sh
+++ Packaging/Linux/install.sh
@@ -36,6 +36,7 @@ cd $ORIG_PATH
 if [ "`uname -s`" != "Darwin" ]; then
     # Install UDEV rules for USB device
     cp ${SCRIPT_PATH}/primesense-usb.rules /etc/udev/rules.d/557-primesense-usb.rules 
+    cp ${SCRIPT_PATH}/orbbec-usb.rules /etc/udev/rules.d/558-orbbec-usb.rules 
 fi
 
 OUT_FILE="$SCRIPT_PATH/OpenNIDevEnvironment"
